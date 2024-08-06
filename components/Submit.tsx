847644900a9dccbe2a9c112dc4f08c6de8a59d2f
JINJUKIM <devpearlkim@gamil.com>
2024-08-06 16:42:01 +0900

    chore: nextui button label typing

--- components/Submit.tsx
+++ components/Submit.tsx
@@ -1,16 +1,20 @@
-'use client'
+"use client";
 
-import { Button } from '@nextui-org/react'
-import { useFormStatus } from 'react-dom'
+import { Button, ButtonProps } from "@nextui-org/react";
+import { useFormStatus } from "react-dom";
 
-const Submit = ({ label, ...btnProps }) => {
-  const { pending } = useFormStatus()
+interface SubmitProps extends ButtonProps {
+  label: string;
+}
+
+const Submit: React.FC<SubmitProps> = ({ label, ...btnProps }) => {
+  const { pending } = useFormStatus();
 
   return (
     <Button {...btnProps} type="submit" isLoading={pending}>
       {label}
     </Button>
-  )
-}
+  );
+};
 
-export default Submit
+export default Submit;
